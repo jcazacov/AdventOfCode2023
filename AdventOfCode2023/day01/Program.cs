825969d36c94c3e81e95e2890d8e84ec0f1a789f
jcazacov <jacob.cazacov@gmail.com>
2023-12-01 23:39:22 +0100

    day01 task 1

---
+++ AdventOfCode2023/day01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Input.txt");
+
+            int sum = 0;
+
+            foreach( string line in lines )
+            {
+                char first = ' ';
+                char last = ' ';
+                foreach(char c in line)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        if (first == ' ')
+                        {
+                            first = c;
+                        }
+                        last = c;
+                    }
+                  
+                }
+                string number = $"{first}{last}";
+                sum += Int32.Parse(number);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
