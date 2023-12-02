f4c5e33f77bbec4f7f4cc802a0f73c8b8ac51c69
jcazacov <jacob.cazacov@gmail.com>
2023-12-02 20:25:18 +0100

    day02 task1

---
+++ AdventOfCode2023/day02/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace day02
+{
+
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+
+            //task 1
+            Dictionary<string, int> colours = new Dictionary<string, int>() {
+             {"red", 12 }, {"green", 13}, {"blue", 14}
+             };
+
+            string[] lines = File.ReadAllLines("Input.txt");
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                bool valid = true;
+                string[] parts = line.Split(':');
+                string[] header = parts[0].Split(" ");
+                int id = int.Parse(header[1]);
+                string[] sets = parts[1].Split(';');
+                foreach (string set in sets)
+                {
+                    string[] pairs = set.Split(',');
+                    foreach (string pair in pairs)
+                    {
+                        string[] values = pair.Split(" ");
+                        if (colours[values[2]] < Int32.Parse(values[1]))
+                        {
+                            valid = false;
+                        }
+                    }
+                }
+                if (valid)
+                {
+                    sum += id;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
