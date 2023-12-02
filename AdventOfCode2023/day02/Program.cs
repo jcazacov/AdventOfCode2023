0258640770823cb7e313105c8ff9d3086e298c1e
jcazacov <jacob.cazacov@gmail.com>
2023-12-02 20:34:06 +0100

    day02 task2

--- AdventOfCode2023/day02/Program.cs
+++ AdventOfCode2023/day02/Program.cs
@@ -5,13 +5,14 @@
     { 
         static void Main(string[] args)
         {
+            string inputFile = "Input.txt";
 
             //task 1
             Dictionary<string, int> colours = new Dictionary<string, int>() {
              {"red", 12 }, {"green", 13}, {"blue", 14}
              };
 
-            string[] lines = File.ReadAllLines("Input.txt");
+            string[] lines = File.ReadAllLines(inputFile);
             int sum = 0;
 
             foreach (string line in lines)
@@ -38,7 +39,38 @@
                     sum += id;
                 }
             }
-            Console.WriteLine(sum);
+            Console.WriteLine($" task 1: {sum}");
+
+            //task2
+            
+
+            lines = File.ReadAllLines(inputFile);
+            sum = 0;
+
+            foreach (string line in lines)
+            {
+                Dictionary<string, int> maxCubes = new Dictionary<string, int>() {
+                {"red", 0 }, {"green", 0}, {"blue", 0}
+                };
+                string[] parts = line.Split(':');
+                string[] header = parts[0].Split(" ");
+                int id = int.Parse(header[1]);
+                string[] sets = parts[1].Split(';');
+                foreach (string set in sets)
+                {
+                    string[] pairs = set.Split(',');
+                    foreach (string pair in pairs)
+                    {
+                        string[] values = pair.Split(" ");
+                        if (maxCubes[values[2]] < Int32.Parse(values[1]))
+                        {
+                            maxCubes[values[2]] = Int32.Parse(values[1]);
+                        }
+                    }
+                }
+                sum += maxCubes["red"] * maxCubes["green"] * maxCubes["blue"];
+            }
+            Console.WriteLine($" task 2: {sum}");
         }
     }
 }
